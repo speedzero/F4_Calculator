6b423fc21a4722d0e1edeed6c792bfcc59f145ad
speedzero <939836402@qq.com>
2019-03-12 00:02:55 +0800

    声明些控件

--- app/src/main/java/com/example/lichang/calculator/MainActivity.java
+++ app/src/main/java/com/example/lichang/calculator/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.lichang.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author F4Group
+ */
 public class MainActivity extends AppCompatActivity {
+    //TODO 声明些控件
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
