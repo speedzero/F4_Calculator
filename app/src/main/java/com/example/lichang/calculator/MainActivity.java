2b6ca3f5f50969babd06d42534572b5a6cfb8c0b
speedzero <939836402@qq.com>
2019-03-12 10:28:51 +0800

    添加了按钮

--- app/src/main/java/com/example/lichang/calculator/MainActivity.java
+++ app/src/main/java/com/example/lichang/calculator/MainActivity.java
@@ -2,17 +2,30 @@ package com.example.lichang.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * @author F4Group
  */
 public class MainActivity extends AppCompatActivity {
+
     //TODO 声明些控件
+    Button button ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.Bt1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"这是一个按钮",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
