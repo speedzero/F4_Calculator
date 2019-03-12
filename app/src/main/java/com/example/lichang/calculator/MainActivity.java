4eb0eb99811cadde8e20e7a0c642f498a6b69329
speedzero <939836402@qq.com>
2019-03-12 15:53:20 +0800

    计算器控件的添加 以及单个按钮的点击测试

--- app/src/main/java/com/example/lichang/calculator/MainActivity.java
+++ app/src/main/java/com/example/lichang/calculator/MainActivity.java
@@ -2,30 +2,43 @@ package com.example.lichang.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 /**
  * @author F4Group
  */
-public class MainActivity extends AppCompatActivity {
+public  class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     //TODO 声明些控件
-    Button button ;
+    Button btNub1 ;
+    TextView showNumber;
+    String number = " " ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.Bt1);
+        showNumber = findViewById(R.id.textShow);
+        btNub1 = findViewById(R.id.Nub1);
+        btNub1.setOnClickListener(this);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"这是一个按钮",Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Nub1:
+                number = number+"1";
+                showNumber.setText(number);
+                break;
+        }
+        Toast.makeText(MainActivity.this,"被点击",Toast.LENGTH_SHORT).show();
 
     }
 }
